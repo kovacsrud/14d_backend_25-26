7b9e9e7d9af3804c3d81177e2bfc161cb534afee
kovacsrud <kovacs.rud@gmail.com>
2025-11-20 09:15:33 +0100

    Település lekérdezése

--- magyarorszag_telepulesei/routes/telepulesRoutes.js
+++ magyarorszag_telepulesei/routes/telepulesRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
 
-const{telepuleslista}=require('../controllers/telepulesContoller');
+const{telepuleslista,telepules}=require('../controllers/telepulesContoller');
 
 router.get('/',telepuleslista);
+router.get('/telepulesnev/:telepulesnev',telepules);
+
 
 
 module.exports=router;
