ff0665ce355b9389d05d955b4b1610b788d4388b
kovacsrud <kovacs.rud@gmail.com>
2026-01-30 11:06:21 +0100

    Kutyák API C# webapi-val

---
+++ KutyakWebAPI/KutyakWebAPI/model/KutyaModelData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KutyakWebAPI.model
+{
+    public class KutyaModelData
+    {
+        public KutyakGoodUniqueContext context;
+        public List<Kutyanevek> Kutyanevek {  get; set; }= new List<Kutyanevek>();
+
+        public KutyaModelData()
+        {
+            context = new KutyakGoodUniqueContext();
+            context.Kutyaneveks.Load();
+            context.Kutyafajtaks.Load();
+            context.Kutyas.Load();
+            Kutyanevek = context.Kutyaneveks.Local.ToList();
+        }
+    }
+}
